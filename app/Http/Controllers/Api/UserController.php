556316c13b12dfa8ab88c84156ac434b03fe841c
imalirezapy <mousaviali1381@gmail.com>
2023-01-30 15:57:45 +0330

    Create login route

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,11 +24,8 @@ class UserController extends Controller
     {
         $filenameWithExt = $request->file('profile')->getClientOriginalName();
         $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-        $filename = str_replace(
-            ['$', '/', '.'],
-            '',
-            Hash::make($filename. now()->timestamp)
-        );
+        $filename = \hash('sha256', $filename . now()->timestamp);
+
         $extension = $request->file('profile')->getClientOriginalExtension();
         $fileNameToStore = $filename.'_'.time().'.'.$extension;
 
@@ -40,14 +40,40 @@ class UserController extends Controller
         }
 
         $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
         $validated['profile'] = $profile ?? null;
 
         $user = User::create($validated);
 
-        $token = $user->createToken($user->email)->plainTextToken;
+        $token = $user->createToken('public')->plainTextToken;
 
         return response()->json([
-            'message' => 'Token created',
+            'message' => 'Token generated',
+            'data' => [
+                'token' => $token
+            ]
+        ], Response::HTTP_OK);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $credentials =  $request->only('email', 'password');
+
+        if (!$user = Auth::attempt($credentials)) {
+            throw new HttpResponseException(response()->json([
+                'message' => 'Validation errors',
+                'data' => [
+                    'email' => ['Email or Password is wrong!']
+                ],
+            ], Response::HTTP_BAD_REQUEST));
+        }
+
+        $user = User::whereEmail($request->email)->first();
+
+        $token = $user->createToken('public')->plainTextToken;
+
+        return response()->json([
+            'message' => 'New Token generated.',
             'data' => [
                 'token' => $token
             ]
---
+++ app/Http/Requests/LoginUserRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|max:255|email',
+            'password' => 'required',
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation errors',
+            'data' => $validator->errors(),
+        ], Response::HTTP_BAD_REQUEST));
+    }
+}
