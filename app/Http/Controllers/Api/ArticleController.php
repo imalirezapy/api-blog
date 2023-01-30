e1eebe59c7cb834ff3e63f6eb0478617652fe016
imalirezapy <mousaviali1381@gmail.com>
2023-01-30 17:54:24 +0330

    config middleware and test authentication

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        dd('oh yeah');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/articles', [ArticleController::class, 'index']);
 });
