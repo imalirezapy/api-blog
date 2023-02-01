6d2a86860421566c5062d686765f93f1497164df
imalirezapy <mousaviali1381@gmail.com>
2023-02-01 10:40:00 +0330

    edit fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'fname',
-        'lname',
+        'first_name',
+        'last_name',
         'email',
         'password',
         'profile',
