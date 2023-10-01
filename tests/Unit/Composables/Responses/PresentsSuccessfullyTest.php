0e895041412ad12c1f3e0321b29bb2ad88954e54
Alireza Mousavi <mousaviali1381@gmail.com>
2023-10-01 12:24:51 +0400

    -+ make unit test for not found http response

---
+++ tests/Unit/Composables/Responses/PresentsSuccessfullyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Composables\Responses;
+
+use Tests\TestCase;
+
+class PresentsSuccessfullyTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Unit/Composables/Responses/ThrowsFailureTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Composables\Responses;
+
+
+use Tests\TestCase;
+
+class ThrowsFailureTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->markTestIncomplete();
+    }
+}
