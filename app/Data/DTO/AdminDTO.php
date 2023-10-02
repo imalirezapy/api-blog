71551fe32cb0bf18b7c0e640b08ff5bad4372eea
Alireza Mousavi <mousaviali1381@gmail.com>
2023-10-02 11:41:21 +0400

    + make AdminDTO

---
+++ app/Data/DTO/AdminDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Data\DTO;
+
+use App\Composables\DTO\HintForFromDTO;
+use DragonCode\SimpleDataTransferObject\DataTransferObject;
+use Illuminate\Support\Carbon;
+
+class AdminDTO extends DataTransferObject
+{
+    use HintForFromDTO;
+
+    public string|null $first_name = null;
+    public string|null $last_name = null;
+    public string|null $email = null;
+    public Carbon|string|null $email_verified_at = null;
+    public ?string $password = null;
+    public ?string $avatar = null;
+    public ?string $avatar_relative = null;
+    public Carbon|string|null $created_at = null;
+    public Carbon|string|null $updated_at = null;
+    public Carbon|string|null $deleted_at = null;
+}
